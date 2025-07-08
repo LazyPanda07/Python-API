1f29bdb421b503bbf6ef8111395f895dcec411b2
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2025-07-08 11:20:39 +0500

    Add loading template from stream

--- src/ContextTemplate.cpp
+++ src/ContextTemplate.cpp
@@ -2,7 +2,31 @@
 
 #include "FacerecService.h"
 
-namespace face_sdk_3divi
+class PythonInputStream : public pbio::stl_wraps::WrapIStream
+{
+private:
+    py::object& binaryReadStream;
+
+public:
+    PythonInputStream(py::object& binaryReadStream) :
+        binaryReadStream(binaryReadStream)
+    {
+        if (!py::hasattr(binaryReadStream, "read") || !py::isinstance<py::function>(binaryReadStream.attr("read")))
+        {
+            throw std::runtime_error("Can't find read function in binary_read_stream object");
+        }
+    }
+
+    virtual void read(char* buffer, size_t size) override
+    {
+        py::bytes bytes = binaryReadStream.attr("read")(size);
+        std::string_view temp = static_cast<std::string_view>(bytes);
+
+        std::copy(temp.begin(), temp.end(), buffer);
+    }
+};
+
+namespace face_sdk
 {
     ContextTemplate::ContextTemplate(pbio::ContextTemplate::Ptr implementation) :
         implementation(implementation)
@@ -19,14 +43,9 @@ namespace face_sdk_3divi
 
     ContextTemplate::ContextTemplate(py::object& binaryReadStream)
     {
-        if (!py::hasattr(binaryReadStream, "read") || !py::isinstance<py::function>(binaryReadStream.attr("read")))
-        {
-            throw std::runtime_error("Can't find read function in binary_read_stream object");
-        }
+        PythonInputStream inputStream(binaryReadStream);
 
-        // TODO: read
-        
-        binaryReadStream.attr("read")();
+        implementation = FacerecService::get()->loadContextTemplate(inputStream);
     }
 
     int32_t ContextTemplate::size() const
@@ -41,7 +60,7 @@ namespace face_sdk_3divi
             throw std::runtime_error("Can't find write function in binary_write_stream object");
         }
         std::ostringstream temp;
-        
+
         implementation->save(temp);
 
         std::string data = temp.str();
